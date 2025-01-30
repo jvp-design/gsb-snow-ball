3725525f521b949aab5daa230280328653e7de82
jeffreypohlmeyer <jeffvp@protonmail.ch>
2025-01-29 20:07:45 -0500

    adding migration script in `db/index.ts`

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -4,7 +4,9 @@ import * as schema from './schema';
 
 import Database from 'better-sqlite3';
 import { drizzle } from 'drizzle-orm/better-sqlite3';
+import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 const client = new Database(env.DATABASE_URL);
 export const db = drizzle(client, { schema });
+migrate(db, { migrationsFolder: 'drizzle' });
