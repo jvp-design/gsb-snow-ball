642975e9b96645a38eaf8e91cb9db6380084b6d9
jeffreypohlmeyer <jeffvp@protonmail.ch>
2025-02-10 17:22:03 -0500

    updating CUTOFF_DATE

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -2,6 +2,6 @@
 import { DateTime } from 'luxon';
 
 export const CUTOFF_DATE = DateTime.fromObject(
-	{ year: 2025, month: 2, day: 11, hour: 0, minute: 0 },
+	{ year: 2025, month: 2, day: 12, hour: 5, minute: 0 },
 	{ zone: 'UTC' }
 );
